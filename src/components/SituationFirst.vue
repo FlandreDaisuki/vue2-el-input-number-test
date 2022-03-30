2f9b529ffbf73a0fc0a04a80a0f12df8ebb0da35
FlandreDaisuki <vbnm123c@gmail.com>
2022-03-30 08:54:05 +0800

    Create 1st situation

---
+++ src/components/SituationFirst.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="situation-first">
+    <el-form ref="formEl" :model="form">
+      <h2>Situation 1st</h2>
+      <p>
+        Just use <code>required</code> in &lt;el-form-item&gt;
+      </p>
+      <div class="form-row">
+        <el-form-item prop="number" required label="0-100">
+          <el-input-number
+            v-model="form.number"
+            controls-position="right"
+            :min="0"
+            :max="100"
+            :step="1"
+          />
+        </el-form-item>
+        <el-button class="ml-4" type="danger" @click="reset">Reset</el-button>
+      </div>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue-demi';
+
+export default {
+  name: 'SituationFirst',
+  setup() {
+    const form = ref({});
+    const formEl = ref(null);
+    const reset = () => {
+      delete form.value.number;
+      formEl.value?.resetFields();
+    };
+    return {
+      form,
+      formEl,
+      reset,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.form-row {
+  display: flex;
+  align-items: flex-start;
+}
+.ml-4 {
+  margin-left: 1rem;
+}
+
+::v-deep .el-form-item {
+  display: inline-flex;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import Vue from 'vue'
+import {
+  Form,
+  Button,
+  FormItem,
+  InputNumber,
+} from 'element-ui';
+
 import App from './App.vue'
+
+Vue.use(Form);
+Vue.use(Button);
+Vue.use(FormItem);
+Vue.use(InputNumber);
 
 Vue.config.productionTip = false
 
